175f32e04dfd1494c6627d645a367762c18c76d9
Chamith Kanchana <chamith.r@healthnet.lk>
2021-04-03 20:39:10 +0530

    Initial commit

---
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -0,0 +1,125 @@
+import React, {useState} from 'react';
+import {
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
+import {db, firebase} from '../../firebase/config';
+
+export default function RegistrationScreen({navigation}) {
+  const [fullName, setFullName] = useState('Chamith Kanchan');
+  const [email, setEmail] = useState('chamithkanchana2@gmail.com');
+  const [password, setPassword] = useState('MyNameIsChamithkanchana');
+  const [confirmPassword, setConfirmPassword] = useState(
+    'MyNameIsChamithkanchana',
+  );
+
+  const onFooterLinkPress = () => {
+    navigation.navigate('Login');
+  };
+
+  const onRegisterPress = () => {
+    if (password && password !== confirmPassword) {
+      Alert.alert("Passwords don't match.");
+      return;
+    }
+    try {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(response => {
+          const uid = response.user.uid;
+          const data = {
+            id: uid,
+            email,
+            fullName,
+          };
+
+          const usersRef = db.collection('users');
+          usersRef
+            .doc(uid)
+            .set(data)
+            .then(() => {
+              navigation.navigate('Home', {user: data});
+            })
+            .catch(error => {
+              console.log('Error insert user', error);
+            });
+        })
+        .catch(error => {
+          console.log('Error create user with email', error);
+        });
+    } catch (error1) {
+      console.log('Functio error', error1);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView
+        style={{flex: 1, width: '100%'}}
+        keyboardShouldPersistTaps="always">
+        <Image
+          style={styles.logo}
+          source={require('../../../assets/icon.png')}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={text => setFullName(text)}
+          value={fullName}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={text => setEmail(text)}
+          value={email}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={text => setPassword(text)}
+          value={password}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Confirm Password"
+          onChangeText={text => setConfirmPassword(text)}
+          value={confirmPassword}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => onRegisterPress()}>
+          <Text style={styles.buttonTitle}>Create account</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Already got an account?{' '}
+            <Text onPress={onFooterLinkPress} style={styles.footerLink}>
+              Log in
+            </Text>
+          </Text>
+        </View>
+      </KeyboardAwareScrollView>
+    </View>
+  );
+}
